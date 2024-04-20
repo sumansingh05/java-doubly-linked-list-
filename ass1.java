a8be732dec12a5589705ce7517751a24d0f23bb6
sumansingh05 <kumarsumansingh7870@gmail.com>
2024-04-20 08:33:30 +0530

    change applied

--- ass1.java
+++ ass1.java
@@ -17,14 +17,60 @@ public class ass1{
 
     public void addFirst(int data){
         Node newNode = new Node(data);
-        if(head == null){            
+        if(head == null){ 
+            head = newNode;           
             tail = newNode;
         }else{
             head.prev = newNode;
-            newNode.next = head;            
-        }
-        head = newNode;
+            newNode.next = head; 
+            head = newNode;           
+        }        
         count += 1;
+    }
+
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){ 
+            head = newNode;           
+            tail = newNode;
+        }else{
+            tail.next = newNode;
+            newNode.prev = tail; 
+            tail = newNode;           
+        }        
+        count += 1;
+    }
+
+    public void deleteFirst(){
+        if(head == null){
+            System.out.println("Doubly Linked List is empty");
+            return;
+        }else if(head.next == null){
+            //only one node in the list
+            head = null;
+            tail = null;
+        }else{
+            //more than one node in the list
+            head = head.next;
+            head.prev = null;
+        }
+        count -= 1;
+    } 
+    
+    public void deleteLast(){
+        if(head == null){
+            System.out.println("Doubly Linked List is empty");
+            return;
+        }else if(head.next == null){
+            //only one node in the list
+            head = null;
+            tail = null;
+        }else{
+            //more than one node in the list
+            tail.prev.next = null; 
+            tail = tail.prev;           
+        }
+        count -= 1;
     }
 
     public void display(){
@@ -40,6 +86,13 @@ public class ass1{
         ass1 dll = new ass1();
         dll.addFirst(2);
         dll.addFirst(1);
+        dll.addLast(10);
+        dll.addLast(11);
+        dll.display();
+        System.out.println("Length of Doubly Linked List is " + dll.count);
+        System.out.println("Head of Doubly Linked List is " + dll.head.data);
+        System.out.println("Tail of Doubly Linked List is " + dll.tail.data);
+        dll.deleteLast();
         dll.display();
         System.out.println("Length of Doubly Linked List is " + dll.count);
         System.out.println("Head of Doubly Linked List is " + dll.head.data);
